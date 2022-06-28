3854d827aad1cca231d20868498173a376b98be0
newmki <owen.ki.dev@gmail.com>
2022-06-28 09:16:01 +0900

    💄 [style] FT-10 Config 폴더 위치 변경

--- srcs/requirements/back/src/app.module.ts
+++ srcs/requirements/back/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import configuration from '../config/configuration';
+import configuration from './config/configuration';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
