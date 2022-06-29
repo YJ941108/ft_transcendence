82d526867a2dc4d79e1f1bde102166985484d051
dennis903 <hyeongjin3@hanmail.net>
2022-06-29 13:58:50 +0900

    [docs]: react 파일 위치 변경 front/ft_transcendence => front

--- srcs/requirements/front/src/App.tsx
+++ srcs/requirements/front/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 		<ThemeProvider theme={mainTheme}>
 			<GlobalStyle />
 			<Router />
-			<ReactQueryDevtools initialIsOpen={true} />
+			<ReactQueryDevtools initialIsOpen />
 		</ThemeProvider>
 	);
 }
