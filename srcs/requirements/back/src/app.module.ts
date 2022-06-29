b6985fe585a4de68eb5077961ee649091e482338
Meongjin Ki <owen.ki.dev@gmail.com>
2022-06-29 13:30:34 +0900

    ✨[feat] FT-17 Health Check module 개발

--- srcs/requirements/back/src/app.module.ts
+++ srcs/requirements/back/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { AuthFortyTwoModule } from './modules/auth-fortytwo/auth-fortytwo.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthFortyTwoModule } from './modules/auth-fortytwo/auth-fortytwo.module
       isGlobal: true,
     }),
     AuthFortyTwoModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
