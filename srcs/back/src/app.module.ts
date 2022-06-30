927fdf084f9c4a17f1ce3c41ed239e88a47526af
Meongjin Ki <owen.ki.dev@gmail.com>
2022-06-30 17:08:02 +0900

    ✨[feat] FT-13 typeorm 적용 중

--- srcs/back/src/app.module.ts
+++ srcs/back/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './config/configuration';
 import { AuthFortyTwoModule } from './modules/auth-fortytwo/auth-fortytwo.module';
 import { HealthModule } from './modules/health/health.module';
@@ -7,6 +8,16 @@ import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'transcendence',
+      password: 'transcendence',
+      database: 'transcendence',
+      entities: [__dirname + '../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
       load: [configuration],
