bd7dc44565a79596b6b8e9a86507794e48c09293
dennis903 <hyeongjin3@hanmail.net>
2022-07-01 12:46:45 +0900

    :hammer: [feat] FT-18 42로그인 버튼 만듬

---
+++ srcs/front/src/modules/constant.ts
@@ -0,0 +1,4 @@
+const LOGIN_API_URL =
+	'https://api.intra.42.fr/oauth/authorize?client_id=113467813cbe779f2287cf962c1283b30173c459fe8b29200557ddc1aab51b1e&redirect_uri=http://127.0.0.1:3032/auth/42/callback&response_type=code';
+
+export default LOGIN_API_URL;
