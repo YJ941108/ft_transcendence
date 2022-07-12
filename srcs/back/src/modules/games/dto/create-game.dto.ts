6ad1a14e3d9473a8d1e311e3235e019d57004667
mki <mki@student.42seoul.kr>
2022-07-12 13:18:54 +0900

    ✨ [feat] FT-41 게임 모듈 dto 작성
    - create-game
    - update-game
    - user-connect

---
+++ srcs/back/src/modules/games/dto/create-game.dto.ts
@@ -0,0 +1,39 @@
+import { IsString, IsOptional, IsInt } from 'class-validator';
+import { Users } from '../../users/users.entity';
+import { GameMode } from '../../../enums/games.enum';
+
+export class CreateGameDto {
+  readonly players: Users[];
+
+  @IsOptional()
+  @IsInt()
+  readonly winnerId: number;
+
+  @IsOptional()
+  @IsInt()
+  readonly loserId: number;
+
+  @IsOptional()
+  @IsInt()
+  readonly winnerScore: number;
+
+  @IsOptional()
+  @IsInt()
+  readonly loserScore: number;
+
+  @IsOptional()
+  @IsString()
+  readonly createdAt: Date;
+
+  @IsOptional()
+  @IsString()
+  readonly endedAt: Date;
+
+  @IsOptional()
+  @IsInt()
+  readonly gameDuration: number;
+
+  @IsOptional()
+  @IsInt()
+  readonly mode: GameMode;
+}
