bdd3c45135de1f6d29acb70cc38927d12e77a132
mki <mki@student.42seoul.kr>
2022-07-12 19:05:53 +0900

    ✨ [feat] FT-34 /api/users/me/tfa - PATCH

--- srcs/back/src/modules/users/users.service.ts
+++ srcs/back/src/modules/users/users.service.ts
@@ -1,24 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Users } from './users.entity';
 import { UsersRepository } from './users.repository';
 
+/**
+ *  @class UsersService
+ */
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(UsersRepository) private usersRepository: UsersRepository) {}
+  private logger = new Logger('UsersService');
 
+  /**
+   * @param usersRepository
+   */
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+
+  /**
+   * 유저 생성
+   * @param createUserDto
+   * @returns Promise<void>
+   */
   async createUser(createUserDto: CreateUserDto): Promise<void> {
     return this.usersRepository.createUser(createUserDto);
   }
 
-  async getUser(email: string): Promise<Users> {
-    const user = this.usersRepository.findOne({ email });
+  /**
+   * 유저 조회
+   * @param email
+   * @returns
+   */
+  async getUserByEmail(email: string): Promise<Users> {
+    const user = await this.usersRepository.findOne({ email });
     return user;
   }
 
+  /**
+   * 닉네임 수정
+   * @param nickname
+   * @returns
+   */
   async getUserByNickname(nickname: string): Promise<Users> {
-    const user = this.usersRepository.findOne({ nickname });
+    const user = await this.usersRepository.findOne({ nickname });
+    return user;
+  }
+
+  /**
+   * two-factor authentication 설정
+   * @param nickname
+   * @param tfa
+   * @returns user object
+   */
+  async setTwoFactorAuthByNickname(nickname: string, tfa: boolean): Promise<Users> {
+    const user = await this.getUserByNickname(nickname);
+
+    this.logger.log(`setTwoFactorAuthByNickname: user.tfa Before = ${user.tfa}`);
+    user.tfa = tfa;
+    this.logger.log(`setTwoFactorAuthByNickname: user.tfa After = ${user.tfa}`);
+    await this.usersRepository.save(user);
     return user;
   }
 }
