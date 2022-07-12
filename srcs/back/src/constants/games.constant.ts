97ebd4b2e54a7eaeaaae35a84e14322eb4fca37c
mki <mki@student.42seoul.kr>
2022-07-12 13:04:49 +0900

    ✨ [feat] FT-41 게임 모듈에 필요한 constant, enum 추가

---
+++ srcs/back/src/constants/games.constant.ts
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+export const canvasWidth: number = 1920;
+export const canvasHeight: number = 1080;
+export const timing: number = 15;
+
+/**
+ *
+ */
+export const playerSpeed = 550;
+export const playerWidth = 30;
+export const playerHeight = 200;
+
+/**
+ *
+ */
+export const ballDefaultRadius = 25;
+export const ballDefaultSpeed = 500;
+export const ballMaxSpeed = 1500;
+export const ballAcceleration = 50;
+
+/**
+ *
+ */
+export const defaultMaxUser = 100;
