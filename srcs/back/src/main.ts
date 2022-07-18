9a137dc2007a7796a77ca03246f7919c73ae38c2
mki <mki@student.42seoul.kr>
2022-07-18 12:39:41 +0900

    🚧 [working] FT-47 서버에 저장된 이미지 불러오기 성공

--- srcs/back/src/main.ts
+++ srcs/back/src/main.ts
@@ -5,28 +5,48 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SocketIoAdapter } from './adapters/socket-io.adapter';
+import { join } from 'path';
 
 async function bootstrap() {
+  /** 로그 */
+  const logger: Logger = new Logger('Main');
+
+  /** Express기반 앱 생성 */
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  /** 환경변수 */
   const configService = app.get(ConfigService);
+
+  /** CORS */
   const origin = configService.get('front.origin');
   app.enableCors({
     origin: [origin],
     methods: ['GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+
+  /** Cookie 미들웨어 추가 */
   app.use(cookieParser());
+
+  /** API 요청시 /api로 시작 */
   app.setGlobalPrefix('api', {
     exclude: [{ path: 'health', method: RequestMethod.GET }],
   });
+
+  /** socket.io 미들웨어 추가 */
   app.useWebSocketAdapter(new SocketIoAdapter(app));
 
+  /** 서버에 저장된 프로필 이미지 요청 가능 */
+  const path = join(__dirname, '..', 'uploads');
+  logger.log(`path: ${path}`);
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    index: false,
+    prefix: '/uploads',
+  });
+
+  /** 소켓 수신 */
   const port = configService.get('port');
-
-  const logger: Logger = new Logger('Main');
   logger.log(`.env PORT=${port}`);
-
   await app.listen(port);
 }
 bootstrap();
--- srcs/back/src/modules/users/users.controller.ts
+++ srcs/back/src/modules/users/users.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Get,
   Logger,
@@ -7,14 +6,15 @@ import {
   Patch,
   Post,
   Req,
+  Res,
   UploadedFile,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Request, Response } from 'express';
 import { multerOptions } from 'src/middleware/multer.middleware';
-import { CreateUserDto } from './dto/create-user.dto';
 import { Users } from './users.entity';
 import { UsersService } from './users.service';
 
@@ -22,14 +22,26 @@ import { UsersService } from './users.service';
 @UseGuards(AuthGuard())
 export class UsersController {
   private logger = new Logger('UsersController');
+
   constructor(private usersService: UsersService) {}
 
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
-    this.logger.log(createUserDto);
-    return this.usersService.createUser(createUserDto);
+  /**
+   * 프로필 사진 불러오기
+   * @param filename
+   * @param res
+   */
+  @Get(':filename')
+  getProfileImage(@Req() req: Request, @Param('filename') filename: string, @Res() res: Response) {
+    this.logger.log(`user: ${JSON.stringify(req.user)}`);
+    this.logger.log(`getProfileImage: ${filename}`);
+    res.sendFile(filename, { root: './uploads' });
   }
 
+  /**
+   * 유저 정보 반환
+   * @param req
+   * @returns
+   */
   @Get('me')
   async getUser(@Req() req: any): Promise<Users> {
     const email = req?.user?.email;
@@ -37,6 +49,12 @@ export class UsersController {
     return user;
   }
 
+  /**
+   * 유저 정보 수정
+   * @param req
+   * @param file
+   * @returns
+   */
   @Post('me')
   @UseInterceptors(FileInterceptor('file', multerOptions))
   async setUser(@Req() req: any, @UploadedFile() file: Express.Multer.File): Promise<Users> {
@@ -44,24 +62,45 @@ export class UsersController {
     return this.usersService.setUser(user.id, file);
   }
 
+  /**
+   * 다른 유저 정보 반환
+   * @param nickname
+   * @returns
+   */
   @Get(':nickname')
   async getUserByNickname(@Param('nickname') nickname: string): Promise<Users> {
     const user = await this.usersService.getUserByNickname(nickname);
     return user;
   }
 
+  /**
+   * two factor authentication 설정
+   * @param req
+   * @returns
+   */
   @Patch('me/tfa')
   async setTwoFactorAuthValid(@Req() req: any): Promise<Users> {
     const user = await this.getUser(req);
     return this.usersService.setTwoFactorAuthValid(user.id, req.body.tfa);
   }
 
+  /**
+   * two factor authentication 코드 메일 전송
+   * @param req
+   * @returns
+   */
   @Get('me/tfa')
   async getTwoFactorAuthCode(@Req() req: any): Promise<Object> {
     const user = await this.getUser(req);
     return this.usersService.getTwoFactorAuthCode(user.id);
   }
 
+  /**
+   * two factor authentication 코드 확인
+   * @param req
+   * @param code
+   * @returns
+   */
   @Get('me/tfa/:code')
   async checkTwoFactorAuthCode(@Req() req: any, @Param('code') code: string): Promise<Object> {
     const user = await this.getUser(req);
