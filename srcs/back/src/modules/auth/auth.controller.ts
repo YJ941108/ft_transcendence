e0bf2e5538241a8556b0f1c4c5814ea1c1492917
mki <mki@student.42seoul.kr>
2022-07-21 13:28:14 +0900

    ✨ [feat] FT-68 이메일 로그인 기능 추가

--- srcs/back/src/modules/auth/auth.controller.ts
+++ srcs/back/src/modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { ConflictException, Controller, Get, Logger, Param, Req, Res, UseGuards } from '@nestjs/common';
+import { ConflictException, Controller, Get, Logger, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
+import { join } from 'path';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersRepository } from '../users/users.repository';
 
@@ -24,24 +25,6 @@ export class AuthController {
     private jwtService: JwtService,
     private configService: ConfigService,
   ) {}
-
-  /**
-   *
-   * @param username
-   * @returns
-   */
-  @Get('login/:username')
-  async login(@Param('username') username: string): Promise<string> {
-    try {
-      const user = await this.userRepository.findOne({ username });
-      this.logger.log(user);
-      const payload = { email: user.email };
-      const accessToken = this.jwtService.sign(payload);
-      return accessToken;
-    } catch (e) {
-      throw new ConflictException(e);
-    }
-  }
 
   /**
    *
@@ -71,5 +54,46 @@ export class AuthController {
     this.logger.log('Client Origin' + origin);
     // response.cookie('access_token', accessToken);
     response.redirect(302, origin + `/auth?access_token=${accessToken}`);
+  }
+
+  @Post('email')
+  async emailCallback(
+    @Req()
+    request,
+    @Res()
+    response: Response,
+  ) {
+    const { email } = request.body;
+    const photo = email;
+    const user = await this.userRepository.findOne({ email });
+    if (!user) {
+      const serverOrigin = this.configService.get<string>('server.origin');
+      this.logger.log(`setUser: serverOrigin: ${serverOrigin}`);
+      const fileLocation = serverOrigin + join('/api/users/profile', 'pong.png');
+      this.logger.log(`setUser: fileLocation: ${fileLocation}`);
+      const createUserDto: CreateUserDto = { username: email, email, photo: fileLocation, nickname: email };
+      await this.userRepository.createUser(createUserDto);
+    }
+    const payload = { email };
+    const accessToken = this.jwtService.sign(payload);
+    response.redirect(302, origin + `/auth?access_token=${accessToken}`);
+  }
+
+  /**
+   *
+   * @param username
+   * @returns
+   */
+  @Get('login/:username')
+  async login(@Param('username') username: string): Promise<string> {
+    try {
+      const user = await this.userRepository.findOne({ username });
+      this.logger.log(user);
+      const payload = { email: user.email };
+      const accessToken = this.jwtService.sign(payload);
+      return accessToken;
+    } catch (e) {
+      throw new ConflictException(e);
+    }
   }
 }
