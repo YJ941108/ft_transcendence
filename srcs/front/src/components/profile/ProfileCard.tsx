279128dbc708891321293730ab872c3c7a189c69
yujeon <yujeon@student.42seoul.kr>
2022-07-22 19:13:58 +0900

    :sparkles: FT-71_ProfileCard생성
    해당 유저의 프로필 정보를 보여주는 화면 구현.
    - 유저 사진보여주기
    - 유저 닉네임표시
    - 유저 수정화면으로 이동하는 버튼 추가
    - 유저 게임 히스토리 공간 배정

---
+++ srcs/front/src/components/profile/ProfileCard.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useQuery } from 'react-query';
+import getUserData from '../../modules/api';
+
+const RootStyled = styled.div`
+	grid-area: ProfileCard;
+	height: 100%;
+	a {
+		display: block;
+		padding: 20px;
+		margin-top: 20px;
+		cursor: pointer;
+	}
+`;
+
+const ProfileCardBox = styled.div`
+	border: 1px solid white;
+	display: inline-block;
+	color: white;
+	padding: 20px;
+	height: 250px;
+	width: 188px;
+	border-radius: 10px;
+	text-align: center;
+	cursor: default;
+	h1 {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		font-weight: 600;
+		font-size: 1.2rem;
+		margin-top: 20px;
+	}
+	img {
+		object-fit: cover;
+		border-radius: 50%;
+		width: 150px;
+		height: 150px;
+		margin-bottom: 0.4rem;
+		background-color: var(--white);
+	}
+`;
+
+interface IUser {
+	access_token: string;
+	created_at: string;
+	email: string;
+	nickname: string;
+	photo: string;
+	updated_at: string;
+	username: string;
+	friends: number[];
+	friends_blocked: number[];
+	friends_request: number[];
+	id: number;
+	jwt: string;
+	refresh_token: string;
+}
+
+function ProfileCard() {
+	const { isLoading, data } = useQuery<IUser>('user', getUserData);
+
+	if (isLoading) return null;
+	return (
+		<RootStyled>
+			<ProfileCardBox>
+				<img src={data?.photo} alt="profile" />
+				<h1>{data?.nickname}</h1>
+				<Link to="/ProfileEdit">USER EDIT</Link>
+			</ProfileCardBox>
+		</RootStyled>
+	);
+}
+
+export default ProfileCard;
--- srcs/front/src/routes/pages/ProfilePage.tsx
+++ srcs/front/src/routes/pages/ProfilePage.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import styled from 'styled-components';
 import MainBox from '../../components/styles/box/MainBox';
 import Navbar from '../../components/ navbar/Navbar';
 import ContentBox from '../../components/styles/box/ContentBox';
 import SideBox from '../../components/styles/box/SideBox';
-import Profile from '../../components/profile/Profile';
+import ProfileCard from '../../components/profile/ProfileCard';
 import isAdmin from '../../modules/login/isAdmin';
+import ProfileContent from '../../components/profile/ProfileContent';
+
+const ProfileDiv = styled.div`
+	display: grid;
+	padding: 30px;
+	width: 100%;
+	height: 100%;
+	//grid-template-rows: 300px 1fr;
+	grid-template-columns: 1fr 4fr;
+	grid-template-areas: '
+	ProfileCard ProfileContent
+	';
+	grid-gap: 10px;
+`;
 
 function ProfilePage() {
 	if (!isAdmin()) return <Navigate to="/login" />;
@@ -13,7 +28,10 @@ function ProfilePage() {
 		<MainBox>
 			<Navbar />
 			<ContentBox>
-				<Profile />
+				<ProfileDiv>
+					<ProfileCard />
+					<ProfileContent />
+				</ProfileDiv>
 			</ContentBox>
 			<SideBox />
 		</MainBox>
