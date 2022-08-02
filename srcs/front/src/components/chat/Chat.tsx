0292183f35285abe52d638e1622d2ef94addd6bb
Junghwan Kim <junghwki@c2r9s5.42seoul.kr>
2022-08-02 17:04:17 +0900

    [feat] FT-89 recoil 추가

--- srcs/front/src/components/chat/Chat.tsx
+++ srcs/front/src/components/chat/Chat.tsx
@@ -1,36 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import ChatHeader from './ChatHeader';
+import { useRecoilValue } from 'recoil';
+import { chatContentC } from '../../modules/atoms';
+import SearchInput from './SearchInput';
 import ChatNav from './ChatNav';
-import AllUserList from './AllUserList';
+import UserList from './UserList';
 import OpenChatList from './OpenChatList';
 import FriendsList from './FriendsList';
 import DirectMessageList from './DirectMessageList';
 
 const ChatC = styled.div`
 	width: 300px;
-	position: absolute;
+	height: 100%;
 	top: 3.5rem;
-	/* background-color: red; */
 `;
 
 interface ISelectComponent {
 	[index: string]: React.ReactNode;
-	AllUserList: React.ReactNode;
+	UserList: React.ReactNode;
 	OpenChatList: React.ReactNode;
 	FriendsList: React.ReactNode;
 	DirectMessageList: React.ReactNode;
 }
 
 function Chat() {
-	const [content, setContent] = useState('AllUserList');
-
-	const changeContent = (event: React.MouseEvent<HTMLButtonElement>) => {
-		setContent(event.currentTarget.name);
-	};
+	const content = useRecoilValue(chatContentC);
 
 	const selectComponent: ISelectComponent = {
-		AllUserList: <AllUserList />,
+		UserList: <UserList />,
 		OpenChatList: <OpenChatList />,
 		FriendsList: <FriendsList />,
 		DirectMessageList: <DirectMessageList />,
@@ -38,9 +35,9 @@ function Chat() {
 
 	return (
 		<ChatC>
-			<ChatHeader />
+			<SearchInput />
 			{selectComponent[content]}
-			<ChatNav func={changeContent} />
+			<ChatNav />
 		</ChatC>
 	);
 }
