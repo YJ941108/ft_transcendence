1c743928fe0f448c43a1d05aafd89bd078e05d9e
Junghwan Kim <junghwki@c2r10s2.42seoul.kr>
2022-08-04 13:27:05 +0900

    [style] interface 수정
    IUserData 추가

--- srcs/front/src/modules/Interfaces/userInterface.ts
+++ srcs/front/src/modules/Interfaces/userInterface.ts
@@ -1,4 +1,17 @@
-export default interface IUser {
+export default interface IUserData {
 	id: number;
+	username: string;
+	email: string;
 	nickname: string;
+	photo: string;
+	tfa: boolean;
+	tfaCode: boolean;
+	isFriend: boolean;
+	wins: number;
+	losses: number;
+	ratio: number;
+	socketId: string;
+	achievement: number;
+	createdAt: string;
+	updatedAt: string;
 }
