1c34f579d428be7627489eca660f4b2249f71bf9
dennis903 <hyeongjin3@hanmail.net>
2022-08-17 19:29:58 +0900

    [feat] FT-114 2FA 인증을 위한 페이지 구현

--- srcs/front/src/components/login/Tfa.tsx
+++ srcs/front/src/components/login/Tfa.tsx
@@ -7,65 +7,58 @@ import IUserData from '../../modules/Interfaces/userInterface';
 
 function Tfa() {
 	const { isLoading, data, error } = useQuery<IUserData>('user', getUserData);
-	const [inputs, setInputs] = useState({
-		code0: '',
-		code1: '',
-		code2: '',
-		code3: '',
-		code4: '',
-		code5: '',
-	});
+	const [input, setInput] = useState('');
+	const [isTfa, setIsTfa] = useState(true);
+	const [isTfaSucceed, setIsTfaSucceed] = useState(false);
 
 	useEffect(() => {
-		const patchCode = async () => {
-			const response = await axios.patch('/api/users/me/tfa');
-			console.log(response, 'res');
-			if (response.data.tfaCode === false) {
-				console.log(response.data.tfaCode, 'patchCode');
-				return <Navigate to="/game" />;
-			}
-			return null;
-		};
-		if (data) patchCode();
+		if (data?.tfa === false) {
+			setIsTfa(false);
+		}
 	}, [data]);
+
+	const getTfaCode = () => {
+		axios.post('/api/users/me/tfa');
+	};
 
 	const onSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
-		const codes = inputs.code0 + inputs.code1 + inputs.code2 + inputs.code3 + inputs.code4 + inputs.code5;
-		const response = await axios.post(`/api/users/me/tfa/${codes}`);
-		console.log(response, 'res');
+		const codes = input;
+		await axios
+			.get(`/api/users/me/tfa/${codes}`)
+			.then(() => {
+				setIsTfaSucceed(true);
+			})
+			.catch(() => alert('Invalid code'));
 	};
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
-		setInputs({
-			...inputs,
-			[name]: value,
-		});
+		const { value } = event.target;
+		setInput(value);
+	};
+
+	const onClickReset = () => {
+		setInput('');
 	};
 
 	if (isLoading) return <h1>loading</h1>;
 	if (error) return <h1>error</h1>;
-	if (data && data.tfa)
-		return (
-			<div>
-				<form onSubmit={onSubmit}>
-					<div>
-						<input name="code0" value={inputs.code0} maxLength={1} onChange={handleChange} />
-						<span>-</span>
-						<input name="code1" value={inputs.code1} maxLength={1} onChange={handleChange} />
-						<span>-</span>
-						<input name="code2" value={inputs.code2} maxLength={1} onChange={handleChange} />
-						<span>-</span>
-						<input name="code3" value={inputs.code3} maxLength={1} onChange={handleChange} />
-						<span>-</span>
-						<input name="code4" value={inputs.code4} maxLength={1} onChange={handleChange} />
-						<span>-</span>
-						<input name="code5" value={inputs.code5} maxLength={1} onChange={handleChange} />
-					</div>
-				</form>
-			</div>
-		);
-	return <Navigate to="/game" />;
+	if (!isTfa || isTfaSucceed) return <Navigate to="/game" />;
+	return (
+		<div>
+			<button type="button" onClick={getTfaCode}>
+				get TFA code
+			</button>
+			<form onSubmit={onSubmit}>
+				<div>
+					<input name="code" value={input} minLength={4} maxLength={4} onChange={handleChange} />
+					<button type="submit">submit</button>
+					<button type="button" onClick={onClickReset}>
+						reset
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Tfa;
