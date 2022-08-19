149f92e47b280fa42d00c1508bcd184d8ab29509
dennis903 <hyeongjin3@hanmail.net>
2022-08-19 13:43:08 +0900

    [style] FT-122 React Router Outlet으로 수정

--- srcs/front/src/routes/pages/LeaderboardPage.tsx
+++ srcs/front/src/routes/pages/LeaderboardPage.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
-import MainBox from '../../components/styles/box/MainBox';
 import Navbar from '../../components/navbar/Navbar';
 import ContentBox from '../../components/styles/box/ContentBox';
-import SideBox from '../../components/styles/box/SideBox';
-import isAdmin from '../../modules/login/isAdmin';
 
 function LeaderboardPage() {
-	if (!isAdmin()) return <Navigate to="/login" />;
 	return (
-		<MainBox>
+		<>
 			<Navbar />
 			<ContentBox>
 				<h1>Leaderboard</h1>
 			</ContentBox>
-			<SideBox />
-		</MainBox>
+		</>
 	);
 }
 
