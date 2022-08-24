55a1fd54880df73d6ca5606cd8b00b1a55bade36
mki <mki@student.42seoul.kr>
2022-08-24 18:39:23 +0900

    [working] 배포 테스트

--- srcs/back/src/modules/users/users.gateway.ts
+++ srcs/back/src/modules/users/users.gateway.ts
@@ -23,7 +23,7 @@ import { UsersService } from './users.service';
  */
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://3.39.20.24:3000',
     methods: ['GET', 'POST'],
   },
   namespace: 'api/users',
