19b9167ec193a89de38cdff9b39ed90831d10960
dennis903 <hyeongjin3@hanmail.net>
2022-08-26 21:06:24 +0900

    [fix] eslint off

--- srcs/front/src/routes/pages/AuthPage.tsx
+++ srcs/front/src/routes/pages/AuthPage.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { useLocation, Navigate } from 'react-router-dom';
+import { useLocation, Navigate, useNavigate } from 'react-router-dom';
 import { onLogin } from '../../modules/login/login';
 import { getUserData } from '../../modules/api';
 
 function AuthPage() {
 	const location = useLocation();
+	const navigate = useNavigate();
 	useEffect(() => {
 		const setLogin = async () => {
 			const token: string = location.search.split('=')[1];
@@ -12,7 +13,12 @@ function AuthPage() {
 			const user = await getUserData();
 			localStorage.setItem('user', JSON.stringify(user));
 		};
+		if (localStorage.getItem('token'))
+			return () => {
+				navigate('/');
+			};
 		setLogin();
+		return () => {};
 	}, [location.search]);
 	return <Navigate to="/tfa" />;
 }
